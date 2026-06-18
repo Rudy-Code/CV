64a951b6227f8c86cffb61870bdc4f56b54cc9a8
Marek <fcmaro.g@gmail.com>
2026-06-19 01:14:02 +0200

    add font, Header section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, fontProviders } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: 'Plus Jakarta Sans',
+			weights: ['300', '400', '500', '600', '700'],
+			subsets: ['latin', 'latin-ext'],
+			cssVariable: '--font-plus-jakarta-sans',
+		},
+	],
+	integrations: [
+		icon({
+			include: {
+				lucide: ['*'],
+			},
+		}),
+	],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+})
---
+++ src/data/index.ts
@@ -0,0 +1,21 @@
+// ==== INTERFACE ====
+
+export interface contactInfo {
+	name: string
+	title: string
+	email: string
+	phone: string
+	city: string
+	portfolio: string
+}
+
+// ==== DATA  ====
+
+export const contactInfo: contactInfo = {
+	name: 'Marek Gondek',
+	title: 'Frontend Developer',
+	email: 'marek@rudycode.pl',
+	phone: '+48665260251',
+	city: 'Bochnia',
+	portfolio: 'rudycode.pl',
+}
